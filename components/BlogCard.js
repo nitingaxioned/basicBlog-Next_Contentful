90a2fd095d6dc8f815ef0a086789e36b447afd54
nitingaxioned <niting@axioned.com>
2023-06-23 19:49:10 +0530

    Created blog listing and detail pages

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,22 +1,43 @@
+// "use client"
 import Link from 'next/link'
+import Image from 'next/image';
+import { parseISO, format } from 'date-fns';
 
 export default function RecipeCard({ blog_post }) {
   const { title, slug, thumbnail, bannerImage, tags, author, content} = blog_post.fields
 
+  const dateString = blog_post.sys.createdAt;
+  const date = parseISO(dateString);
+  
   return (
     <li className="card">
       <div className="featured">
-        {/* featured image */}
+        <Image
+          src={'https:' + thumbnail.fields.file.url}
+          width={thumbnail.fields.file.details.image.width}
+          height={thumbnail.fields.file.details.image.height}
+        />
       </div>
       <div className="content">
         <div className="info">
-          <h4>{ title }</h4>
+          <h2>{ title }</h2>
+          <span>Publised on <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time> </span>
+          <br/>
+          <span>Author : { author } </span>
+        
           {/* <p>{ content }</p> */}
         </div>
+        <br/>
         <div className="actions">
-          <Link href="#">Read More</Link>
+          <Link href={'/blog/' + slug}>Read More</Link>
         </div>
       </div>
+
+      <style jsx>{`
+       .card {
+          padding: 20px 0;
+        }
+      `}</style>
     </li>
   )
 }
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,7 +1,105 @@
-export default function BlogDetails() {
-    return (
-      <div>
-        Blog Details
+import { createClient } from 'contentful'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Image from 'next/image'
+import { parseISO, format } from 'date-fns';
+
+const client = createClient({
+  space: 'yo4b4xfnucbc',
+  accessToken: 'Z_HMbg_0DEpNau877P3gRqKlPSkAZAc8eGekbsZXTLo',
+})
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({ 
+    content_type: "blog_post" 
+  })
+
+  const paths = res.items.map(item => {
+    return {
+      params: { slug: item.fields.slug }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps = async ({ params }) => {
+  const { items } = await client.getEntries({
+    content_type: 'blog_post',
+    'fields.slug': params.slug
+  })
+
+  return {
+    props: { blog_post: items[0] }
+  }
+
+}
+
+export default function BlogDetails({blog_post}) {
+  const { title, slug, thumbnail, bannerImage, tags, author, content} = blog_post.fields
+
+  const dateString = blog_post.sys.createdAt;
+  const date = parseISO(dateString);
+
+  return (
+    <div>
+      <div className="banner">
+        <Image 
+          src={'https:' + bannerImage.fields.file.url}
+          width={bannerImage.fields.file.details.image.width}
+          height={bannerImage.fields.file.details.image.height}
+          className="banner-img"
+        />
       </div>
+
+      <div className="info">
+        <h2>{ title }</h2>
+        <span>Publised on <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time> </span>
+        <br/>
+        <span>Author : { author } </span>
+        <div className="blog-tags">{tags.map(tag => (
+          <>
+          <h4>Tags -</h4>
+          <span className="blog-tag" key={tag}>{ tag }</span>
+          </>
+        ))}
+        </div>
+      </div>
+        
+      <div className="content">
+        <div>{documentToReactComponents(content)}</div>
+        <Image
+          src={'https:' + thumbnail.fields.file.url}
+          width={thumbnail.fields.file.details.image.width}
+          height={thumbnail.fields.file.details.image.height}
+        />
+      </div>
+
+      <style jsx>{`
+        .banner {
+          width: 125%;
+          position: relative;
+          left: -10vw;
+        } 
+
+        h4 {
+          margin-block-end: 1.33em;
+        }
+
+        .blog-tag {
+          padding: 3px 7px;
+          display: inline-block;
+          background: #eee;
+          border-radius: 7px;
+          margin: 0 10px 10px 0;
+        }
+
+        .content {
+          max-width: 800px;
+        }
+      `}</style>
+    </div>
     )
 }
